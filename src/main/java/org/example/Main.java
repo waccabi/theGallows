d83381e22c37255095ea262f0bd6a0ddf18aa521
Maksim Ruf <max.progg@yandex.ru>
2024-01-04 16:27:40 +0700

    Test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 5;
+        int b = 6;
+        int c = a+b;
+        System.out.println(c);
+
+
     }
 }
