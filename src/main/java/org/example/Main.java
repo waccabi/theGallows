1ea2e7a39ad2bdd3d659e203777b273041c57ee1
Maksim Ruf <max.progg@yandex.ru>
2024-01-05 21:58:53 +0700

    Написал, выбор  конкретного слова из текст файла

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a = 5;
-        int b = 6;
-        int c = a+b;
-        System.out.println(c);
-
-
+    public static void main(String args[]) throws Exception{
+        FileReader lib = new FileReader("src\\main\\resources\\lib.txt");
+        Scanner s = new Scanner(lib);
+        int i = 1;
+        while (s.hasNextLine()){
+            String word = s.nextLine();
+            if (i == 7){
+                System.out.println(word);
+                break;
+            }
+            i++;
+        }
+        lib.close();
     }
 }
